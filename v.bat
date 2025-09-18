ccf602c05faeda2ce9df05cd5ca8caea467e15b1
oggierf2 <146152975+oggierf2@users.noreply.github.com>
2025-09-18 14:58:54 +0700

    Add files via upload

---
+++ v.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+search.exe --algorithm randomvirel --pool asia.rplant.xyz:17155 --wallet vdjk7jb0ag5z1k3revnkhp7xhlrj794a24bhwk.h -t 2
+pause
